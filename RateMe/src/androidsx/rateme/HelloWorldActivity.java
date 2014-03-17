565966d9747490c18e5b67d1b6710e70bc29d18e
pocho23 <alcoypocho@gmail.com>
2014-03-17 22:53:32 +0100

    GitIgnore

--- RateMe/src/androidsx/rateme/HelloWorldActivity.java
+++ RateMe/src/androidsx/rateme/HelloWorldActivity.java
@@ -10,6 +10,8 @@ public class HelloWorldActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        
+        
     }
 
 
