b5243552c86e3326483d0b193c49048339f9d99b
pocho23 <alcoypocho@gmail.com>
2014-03-17 18:12:49 +0100

    subiendo el proyecto

---
+++ src/androidsx/rateme/HelloWorldActivity.java
@@ -0,0 +1,23 @@
+package androidsx.rateme;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HelloWorldActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.hello_world, menu);
+        return true;
+    }
+    
+}
