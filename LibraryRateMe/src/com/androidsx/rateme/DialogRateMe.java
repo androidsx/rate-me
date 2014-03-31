bd23dcf07ea6c50737c473bcb90a46d18afdcdf4
Pocho23 <alcoypocho@gmail.com>
2014-03-31 15:17:36 -0400

    Implements Code Button

--- LibraryRateMe/src/com/androidsx/rateme/DialogRateMe.java
+++ LibraryRateMe/src/com/androidsx/rateme/DialogRateMe.java
@@ -1,7 +1,5 @@
 package com.androidsx.rateme;
 
-import com.androidsx.libraryrateme.R;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -15,56 +13,74 @@ import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
+import com.androidsx.libraryrateme.R;
+
 public class DialogRateMe extends DialogFragment {
-	
+
 	private String appPackageName;
 	private View mView;
-    private View tView;
-    private Button close;
-    private RatingBar ratingBar;
-    private LayerDrawable stars;
-	
-    public static DialogRateMe newInstance(String appName) {
-    	DialogRateMe dialogo = new DialogRateMe();      
-        Bundle args = new Bundle();
-        args.putString("name", appName);
-        dialogo.setArguments(args);
-        return dialogo;
-    }
-    
+	private View tView;
+	private Button close;
+	private RatingBar ratingBar;
+	private LayerDrawable stars;
+	private Button RateMe;
+	private Button NoThanks;
+
+	public static DialogRateMe newInstance(String appName) {
+		DialogRateMe dialogo = new DialogRateMe();
+		Bundle args = new Bundle();
+		args.putString("name", appName);
+		dialogo.setArguments(args);
+		return dialogo;
+	}
+
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
+
 		appPackageName = getArguments().getString("name");
-		
+
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		mView = getActivity().getLayoutInflater().inflate(R.layout.library, null);
-        tView = getActivity().getLayoutInflater().inflate(R.layout.title, null);
-        close = (Button)tView.findViewById(R.id.cerrar);
-        
-        ratingBar = (RatingBar) mView.findViewById(R.id.ratingBar);
-        stars = (LayerDrawable) ratingBar.getProgressDrawable();
-        stars.getDrawable(2).setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Toast.makeText(getActivity(), "Puntuacion: " + String.valueOf(rating), Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        
-        close.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dismiss();
-
-            }
-        });
+		mView = getActivity().getLayoutInflater().inflate(R.layout.library,
+				null);
+		tView = getActivity().getLayoutInflater().inflate(R.layout.title, null);
+		close = (Button) tView.findViewById(R.id.cerrar);
+		RateMe = (Button) tView.findViewById(R.id.buttonRateMe);
+		NoThanks = (Button) tView.findViewById(R.id.buttonThanks);
 		
+
+		ratingBar = (RatingBar) mView.findViewById(R.id.ratingBar);
+		stars = (LayerDrawable) ratingBar.getProgressDrawable();
+		stars.getDrawable(2).setColorFilter(Color.YELLOW,
+				PorterDuff.Mode.SRC_ATOP);
+		ratingBar
+				.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+
+					@Override
+					public void onRatingChanged(RatingBar ratingBar,
+							float rating, boolean fromUser) {
+						Toast.makeText(getActivity(),
+								"Puntuacion: " + String.valueOf(rating),
+								Toast.LENGTH_SHORT).show();
+						if (rating >= 4.0) {
+							RateMe.setVisibility(View.VISIBLE);
+						}else{
+							NoThanks.setVisibility(View.VISIBLE);
+						}
+					}
+				});
+		close.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dismiss();
+
+			}
+		});
+
 		return builder
 				.setView(mView)
 				.setCustomTitle(tView)
 				.setCancelable(false)
 				.create();
+
 	}
 }
