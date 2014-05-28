2ed9e0c09548eb17f2d9bb6b2d5cf022df52d6e2
pocho23 <alcoypocho@gmail.com>
2014-05-28 23:18:36 +0200

    add onStart in RateMe Project

--- RateMe/src/com/androidsx/rateme/demo2/HelloWorldActivity.java
+++ RateMe/src/com/androidsx/rateme/demo2/HelloWorldActivity.java
@@ -11,6 +11,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import androidsx.rateme.R;
 
+import com.androidsx.rateme.CustomShowDialog;
 import com.androidsx.rateme.DialogRateMe;
 
 public class HelloWorldActivity extends Activity {
@@ -56,6 +57,20 @@ public class HelloWorldActivity extends Activity {
     }
     
     
+    
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        CustomShowDialog.onStart(this);
+        if (CustomShowDialog.shouldShowRateDialog(this)) {
+            DialogFragment dialogo = DialogRateMe.newInstance(
+                    myPackageName);
+            dialogo.show(getFragmentManager(), "dialog");
+        }
+        
+    }
+
     public void alertMenu (){
         
 //        DialogFragment dialogo = DialogRateMe.newInstance(
