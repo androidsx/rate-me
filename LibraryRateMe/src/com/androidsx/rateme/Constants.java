206d22fb1597b3a8aa3115c84382e57cbc267aef
Lucas Ponzoda Vaño (Pocho) <alcoypocho@gmail.com>
2014-06-05 18:48:36 +0200

    Delete Constants.java

--- LibraryRateMe/src/com/androidsx/rateme/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.androidsx.rateme;
-
-public class Constants {
-    
-    public static final String EMAIL = "yourmail@mail.com";
-    public static final int INSTALL_DAYS = 20;
-    public static final int LAUNCH_TIMES = 5;
-
-}
