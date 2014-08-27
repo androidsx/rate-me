7c94b8317742e02030adce39333a7d1d729f9603
pocho23 <alcoypocho@gmail.com>
2014-08-27 11:14:59 +0200

    delete imports

--- LibraryRateMe/src/com/androidsx/rateme/DialogRateMe.java
+++ LibraryRateMe/src/com/androidsx/rateme/DialogRateMe.java
@@ -1,7 +1,5 @@
 package com.androidsx.rateme;
 
-import java.io.ObjectInputStream.GetField;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -17,7 +15,6 @@ import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.GetChars;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
