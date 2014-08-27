d8e23d16fed393db47c84df66086f8ae765a8ea8
pocho23 <alcoypocho@gmail.com>
2014-08-27 18:22:41 +0200

    change if to build

--- LibraryRateMe/src/com/androidsx/rateme/DialogRateMe.java
+++ LibraryRateMe/src/com/androidsx/rateme/DialogRateMe.java
@@ -223,16 +223,17 @@ public class DialogRateMe extends DialogFragment {
     private Dialog confirmGoToMailDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setCustomTitle(confirDialogView).setCancelable(false).setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                goToMail();
-                dismiss();
-            }
-        }).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                dismiss();
-            }
-        });
+        builder.setCustomTitle(confirDialogView).setCancelable(false)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        goToMail();
+                        dismiss();
+                    }
+                }).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dismiss();
+                    }
+                });
         return builder.create();
 
     }
@@ -293,12 +294,9 @@ public class DialogRateMe extends DialogFragment {
 
         public Builder setGoToMail(boolean goToMail) {
             this.goToMail = goToMail;
-            if(goToMail && email == null){
-                throw new IllegalArgumentException("You Have to configure the email for the dialog goToMail");
-            }
             return this;
         }
-        
+
         public Builder setTitleTextColor(int titleTextColor) {
             this.titleTextColor = titleTextColor;
             return this;
@@ -365,6 +363,9 @@ public class DialogRateMe extends DialogFragment {
         }
 
         public DialogRateMe build() {
+            if (goToMail && email == null) {
+                throw new IllegalArgumentException("You Have to configure the email for the dialog goToMail");
+            }
             return new DialogRateMe(this);
         }
 
