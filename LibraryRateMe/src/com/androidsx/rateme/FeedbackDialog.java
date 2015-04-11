3e3f1717c344eb64a18c561447a78ea08b7b9ef4
pocho23 <alcoypocho@gmail.com>
2015-04-11 19:47:55 +0200

    fix correct position line divide color

--- LibraryRateMe/src/com/androidsx/rateme/FeedbackDialog.java
+++ LibraryRateMe/src/com/androidsx/rateme/FeedbackDialog.java
@@ -6,7 +6,6 @@ import android.app.DialogFragment;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Parcel;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -47,9 +46,9 @@ public class FeedbackDialog extends DialogFragment {
                                              int headerTextColor,
                                              int textColor,
                                              int logoResId,
+                                             int lineDividerColor,
                                              int rateButtonTextColor,
                                              int rateButtonBackgroundColor,
-                                             int lineDividerColor,
                                              float getRatingBar,
                                              OnRatingListener onRatingListener) {
         FeedbackDialog feedbackDialog = new FeedbackDialog();
